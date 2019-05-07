461ea625fca732fb8bfcccccb96d5fbf5a12d2bf
thomasneo <thomaslossio@hotmail.com>
2019-05-06 21:47:33 -0300

    Iniciando estrutura

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./config/ReactotronConfig";
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
